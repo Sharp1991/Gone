9fd5f21d79279efe562cefc7903e8c0cb4d391ab
Sharp1991 <shaphrang.syiem@gmail.com>
2026-07-08 10:00:21 +0530

    Update homepage

--- app/hidden-meghalaya/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HiddenMeghalaya() {
-  return (
-    <main style={{ padding: "40px" }}>
-      <h1>Hidden Meghalaya</h1>
-      <p>Discover authentic homestays, stories, and experiences.</p>
-    </main>
-  );
-}
